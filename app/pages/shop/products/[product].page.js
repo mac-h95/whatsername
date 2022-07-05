d6305ecb3dbcb698f9ec0d4c52751f68d5201fb7
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-05 15:10:35 +0100

    Update [product].page.js

--- app/pages/shop/products/[product].page.js
+++ app/pages/shop/products/[product].page.js
@@ -46,7 +46,7 @@ const Product = ({ product }) => {
   return (
     <>
       {/* <Navigation /> */}
-      <div className="flex items-start justify-center space-x-4">
+      <div className="flex-col items-start justify-center space-y-4 md:flex md:space-x-4">
         <DetailsPanel
           {...product}
           image={product.images[0]}
