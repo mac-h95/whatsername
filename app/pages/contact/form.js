91a061e2df9d426de839bed64c09a857eb1e8f5c
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-08 18:02:52 +0100

    Create form.js

--- app/pages/contact/form.js
+++ app/pages/contact/form.js
@@ -1,19 +1,19 @@
 const Form = () => (
-  <form>
+  <form className='flex flex-col items-center justify-center max-w-sm space-y-8 text-center'>
     <div className='my-2'>
-      <label className='block text-gray-700 text-sm font-bold mb-2' for='name'>Name</label>
+      <label className='block text-sm font-bold' for='name'>Name</label>
       <input name='name' type='text' />
     </div>
       <div className='my-2'>
-      <label className='block text-gray-700 text-sm font-bold mb-2' for='email'>Email</label>
+      <label className='block text-sm font-bold' for='email'>Email</label>
       <input name='email' type='email' />
     </div>
     <div className='my-2'>
-      <label className='block text-gray-700 text-sm font-bold mb-2' for='subject'>Subject</label>
+      <label className='block text-sm font-bold' for='subject'>Subject</label>
       <input name='subject' type='text' />
     </div>
     <div className='my-2'>
-      <label className='block text-gray-700 text-sm font-bold mb-2' for='message'>Message</label>
+      <label className='block text-sm font-bold' for='message'>Message</label>
       <textarea name='name' />
     </div>
   </form>
