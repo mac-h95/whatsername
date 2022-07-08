ce106c41ab3ba9c890d03ffa1a564a623e6a89c5
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-08 15:29:00 +0000

    events/list.js updated to tidy mobile styles

--- app/pages/events/list.js
+++ app/pages/events/list.js
@@ -1,12 +1,12 @@
-import { getDateString } from 'date';
-import Image from 'next/image';
-import Link from 'next/link';
-import { urlFor } from 'sanity';
+import { getDateString } from "date";
+import Image from "next/image";
+import Link from "next/link";
+import { urlFor } from "sanity";
 
 const Event = ({ tickets, image, title, venue, date }) => (
   <Link href={tickets} passHref>
     <a
-      className="flex flex-col items-center justify-center space-y-2 my-9 mr-9"
+      className="flex flex-col items-center justify-center space-y-2 md:my-9 md:mr-9"
       target="_blank"
       rel="noopener"
     >
@@ -31,18 +31,20 @@ const EventsList = ({ events }) => {
 
   return (
     <div className="flex flex-col items-start space-y-8 md:px-32 md:w-screen">
-      <div className="flex flex-col items-center space-y-8 md:justify-start md:space-y-0 md:flex-wrap md:flex-row ">
+      <div className="flex flex-col items-center space-y-8 md:justify-start md:space-y-0 md:flex-wrap md:flex-row">
         {upcomingEvents.map((event) => (
           <Event key={event._id} {...event} />
         ))}
       </div>
-      <div className="">
+      <div>
         <h2 className="mb-3 text-2xl font-semibold tracking-widest text-center md:text-left md:flex-wrap text-background-300">
           Past Events
         </h2>
-        {pastEvents.map((event) => (
-          <Event key={event._id} {...event} />
-        ))}
+        <div className="flex flex-col items-center space-y-8 md:justify-start md:space-y-0 md:flex-wrap md:flex-row ">
+          {pastEvents.map((event) => (
+            <Event key={event._id} {...event} />
+          ))}
+        </div>
       </div>
     </div>
   );
