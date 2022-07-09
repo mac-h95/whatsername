2aa6039e669a228ce93d239483fab8cd74d4edcd
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-09 10:47:56 +0000

    updated author.js to look better on mobile styles

--- app/pages/blog/posts/author.js
+++ app/pages/blog/posts/author.js
@@ -5,15 +5,15 @@ import { urlFor } from 'sanity';
 const Author = ({ link, name, image, role }) => (
   <Link href={link}>
     <a
-      className="flex items-center space-x-2 no-underline text-foreground-500"
+      className="flex items-center max-w-xs space-x-2 no-underline text-foreground-500"
       target="_blank"
       rel="noopener"
     >
       <Image
         src={urlFor(image)}
         alt={name}
-        width={60}
-        height={60}
+        width={100}
+        height={100}
         style={{ borderRadius: '50%' }}
       />
       <div className="flex flex-col text-left">
