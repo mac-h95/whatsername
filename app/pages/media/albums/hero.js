0f76181ac190a5f76b924c34db31cb072f76e663
Mac <5686378+mac-h95@users.noreply.github.com>
2022-07-26 11:36:56 +0000

    Basic site complete TODO: - [ ] Set default value for options fields - [ ] Add dashboard for managing orders

--- app/pages/media/albums/hero.js
+++ app/pages/media/albums/hero.js
@@ -2,11 +2,12 @@ import { getFullDateString } from 'date';
 import Image from 'next/image';
 import Link from 'next/link';
 import { urlFor } from 'sanity';
+import NextHead from 'next/head';
 
 const Hero = ({ cover, artist, venue, date }) => (
   <div className="grid w-screen min-h-screen place-items-center">
     <NextHead>
-      <title>{`${artist} - ${venue} - ${date} | Whatsername`}</title>
+      <title>{`${artist} at ${venue.name} - ${date} | Whatsername`}</title>
     </NextHead>
     <Image src={urlFor(cover)} alt={artist} layout="fill" objectFit="cover" />
     <div className="z-40 flex flex-col items-center pb-64 ">
